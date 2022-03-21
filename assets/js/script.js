565dba0bfd53de845fa0eb8d682b6aa8029f179a
DaviEmmanuel04 <davi.emannuellre@gmail.com>
2022-03-20 21:20:35 -0300

    Corrigindo os alertas de erro do form e adicionando a área de opinião

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,32 +6,44 @@ function verifica() {
     let valida = 0
 
     if(email.value === ""){
-        alert("O campo email deve ser preenchido");
+        document.querySelector("#erro_email").classList.toggle("visivel")
+        return 0;
     }else{
         valida += 1;
     }
 
     // Validações para o email
+    let qtd = 0;
+    for(let i = 0; i < email.value.length; i++){
+        if(email.value[i] === "@"){
+            qtd++;
+        }
+    }
+    const maisArroba = qtd > 1;
     const semArroba = email.value.indexOf("@") === -1;
     const semPonto = email.value.indexOf(".") === -1;
     const semDominio = email.value.indexOf(".") - email.value.indexOf("@") === 1 || email.value.indexOf(".") - email.value.indexOf("@") >17 ;
     const semUser = email.value.indexOf("@") === 0;
     const userGrande = email.value.indexOf("@") > 32;
-    if (semArroba || semPonto || semDominio || semUser || userGrande){
-        alert("Email Inválido");
+    if (semArroba || semPonto || semDominio || semUser || userGrande || maisArroba){
+        document.querySelector("#erro_email").classList.toggle("visivel")
+        return 0;
     }else{
         valida += 1;
     }
 
     //sss
     if(msg.value === ""){
-        alert("O campo mensagem deve ser preechido.");
+        //alert("O campo mensagem deve ser preechido.");
+        document.querySelector("#erro_email").classList.remove("visivel")
+        document.querySelector("#erro_mensagem").classList.toggle("visivel")
+        return 0;
     }else{
         valida += 1;
     }
 
     if(valida === 3){
-        const user = email.value.substr(0, email.value.indexOf("@"))
+        const user = email.value.substr(0, email.value.indexOf("@"));
         alert(`Obrigado pelo contato, ${user} ;D`);
     }
 
