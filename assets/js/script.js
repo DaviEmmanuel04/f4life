012a20de55db6e12d6930f6f98646eda08fb0c08
DaviEmmanuel04 <davi.emannuellre@gmail.com>
2022-03-20 22:59:11 -0300

    Corrigindo os alertas de erro

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,13 @@ function verifica() {
     }
     const maisArroba = qtd > 1;
     const semArroba = email.value.indexOf("@") === -1;
-    const semPonto = email.value.indexOf(".") === -1;
+    const semPonto = email.value.substr(email.value.indexOf("@")).indexOf(".") === -1;
+    const nadaPosPonto = (email.value.substr(email.value.indexOf("@")).length - email.value.substr(email.value.indexOf("@")).indexOf(".")) === 1;
     const semDominio = email.value.indexOf(".") - email.value.indexOf("@") === 1 || email.value.indexOf(".") - email.value.indexOf("@") >17 ;
     const semUser = email.value.indexOf("@") === 0;
     const userGrande = email.value.indexOf("@") > 32;
-    if (semArroba || semPonto || semDominio || semUser || userGrande || maisArroba){
+    if (semArroba || semPonto || semDominio || semUser || userGrande || maisArroba || nadaPosPonto){
+        email.value = "";
         document.querySelector("#erro_email").classList.toggle("visivel")
         return 0;
     }else{
@@ -33,18 +35,22 @@ function verifica() {
     }
 
     //sss
-    if(msg.value === ""){
+    if(msg.value == false){
         //alert("O campo mensagem deve ser preechido.");
+        msg.value = "";
         document.querySelector("#erro_email").classList.remove("visivel")
         document.querySelector("#erro_mensagem").classList.toggle("visivel")
-        return 0;
+        msg.focus();
+        
     }else{
         valida += 1;
     }
 
     if(valida === 3){
         const user = email.value.substr(0, email.value.indexOf("@"));
-        alert(`Obrigado pelo contato, ${user} ;D`);
+        const mensagem = `Obrigado pelo contato, ${user} ;D`
+        msg.value = "";
+        document.getElementById("enviou").innerHTML = mensagem;
     }
 
 }
